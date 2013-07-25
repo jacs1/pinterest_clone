4e0dfbcd35c5addda2e498110ad2f419fdb01b0f
Joseph Simoes <josephsimoes@Victors-MacBook-Pro.local>
2013-07-24 23:06:12 -0400

    Created pages controller and implemented bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinterestClone::Application.config.secret_token = '93db3a48f3090b4e873af0a10bd9601da477c61e8b66625f1a6bd793cb37b9c2ad0358776699e74d077404446c69c3afddbe194d96252a741361e1e0162b2f71'
